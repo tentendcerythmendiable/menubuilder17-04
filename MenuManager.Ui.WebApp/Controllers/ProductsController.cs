0864e859663d8287ce8e120f266246540f6d104a
tentendcerythmendiable <125271192+tentendcerythmendiable@users.noreply.github.com>
2023-04-18 12:42:42 +0200

    Initial commit

---
+++ MenuManager.Ui.WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using MenuManager.Ui.WebApp.Core;
+using MenuManager.Ui.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuManager.Ui.WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly MenuManagerDbContext _menuManagerDbContext;
+
+        public ProductsController(MenuManagerDbContext menuManagerDbContext)
+        {
+            _menuManagerDbContext = menuManagerDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var products = _menuManagerDbContext.Products.ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            product.CreatedDate = DateTime.Now;
+
+            _menuManagerDbContext.Products.Add(product);
+            _menuManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _menuManagerDbContext.Products
+                .SingleOrDefault(p => p.Id == id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            var dbProduct = _menuManagerDbContext.Products
+                .SingleOrDefault(p => p.Id == product.Id);
+
+            if (dbProduct is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            dbProduct.Name = product.Name;
+            dbProduct.Description = product.Description;
+            dbProduct.Price = product.Price;
+
+            _menuManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
